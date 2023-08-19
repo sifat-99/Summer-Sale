464831dd6f3656f873bcc61b5e77646fba87d69a
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-08-20 00:01:46 +0600

    javaScript Applied

--- js/allCalculation.js
+++ js/allCalculation.js
@@ -1,1 +1,19 @@
-console.log("accepted")
+// console.log("accepted")
+let sum= 0;
+function calculateAllPrice(card)
+{
+    
+    const partiCularPriceString = card.parentNode.childNodes[3].childNodes[3].childNodes[5].childNodes[1].innerText;
+    const partiCularItemsPriceFirst = parseFloat(partiCularPriceString);
+    sum = sum+partiCularItemsPriceFirst;
+    toShow(sum);
+}
+function toShow(sum)
+{
+    const updateBoxName = document.getElementById("PriceInput")
+    const li = document.createElement("li");
+    sum = sum.toFixed(2);
+    li.innerText = sum;
+    updateBoxName.innerHTML="Total price: "+sum;
+    // console.log(sum)
+}
