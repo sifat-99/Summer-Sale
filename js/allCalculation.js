f3012c5897e07807b9a1d8bacab2b6836fac387a
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-08-20 01:18:02 +0600

    summation added

--- js/allCalculation.js
+++ js/allCalculation.js
@@ -1,4 +1,3 @@
-// console.log("accepted")
 let sum= 0;
 function calculateAllPrice(card)
 {
@@ -6,14 +5,53 @@ function calculateAllPrice(card)
     const partiCularPriceString = card.parentNode.childNodes[3].childNodes[3].childNodes[5].childNodes[1].innerText;
     const partiCularItemsPriceFirst = parseFloat(partiCularPriceString);
     sum = sum+partiCularItemsPriceFirst;
+    // InputValue()
+    // const DiscountPrice = discount(sum);
     toShow(sum);
+    
 }
 function toShow(sum)
 {
     const updateBoxName = document.getElementById("PriceInput")
     const li = document.createElement("li");
     sum = sum.toFixed(2);
+    
     li.innerText = sum;
     updateBoxName.innerHTML="Total price: "+sum;
-    // console.log(sum)
+    // const li2 = document.createElement("li");
+    // DiscountPrice = DiscountPrice.toFixed(2);
+    // li2.innerText = DiscountPrice;
+    // updateBoxName.innerHTML = "Total: " + DiscountPrice;
+    console.log(sum)
+    
 }
+
+function InputValue()
+{
+    const inputHTML = document.getElementById("InputField");
+    const input = inputHTML.value;
+    const btnActive = inputHTML.parentNode.childNodes[3];
+    if(input === "SELL200")
+    {
+        btnActive.removeAttribute('disabled');
+    }
+    else
+    {
+        btnActive.disabled = true;
+    }
+}
+
+// document.getElementById("coupon-btn").addEventListener('click',function discount(value){
+//         if(value >0)
+//         {
+//             const discountValue = (value/100)*20;
+//             const total = value - discountValue;
+//             return total;
+//         }
+    
+// })
+
+// function setToThePurchaseField(total)
+// {
+
+// }
