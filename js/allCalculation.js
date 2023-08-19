b30f0b587acc717abf632c787c82b6395bed80d2
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-08-20 04:43:01 +0600

    Data Showed

--- js/allCalculation.js
+++ js/allCalculation.js
@@ -1,29 +1,61 @@
+// document.getElementById("Card-4").addEventListener('click', function(){
+//     calculateAllPrice("Card-4");
+
+// })
 let sum= 0;
 function calculateAllPrice(card)
 {
     
-    const partiCularPriceString = card.parentNode.childNodes[3].childNodes[3].childNodes[5].childNodes[1].innerText;
+    const partiCularPriceString = card.childNodes[3].childNodes[5].childNodes[1].innerText;
+    const particularUnitName = card.childNodes[3].childNodes[3].innerText;
+    console.log(particularUnitName)
     const partiCularItemsPriceFirst = parseFloat(partiCularPriceString);
     sum = sum+partiCularItemsPriceFirst;
-    // InputValue()
-    // const DiscountPrice = discount(sum);
-    toShow(sum);
+    document.getElementById("coupon-btn").addEventListener('click',function(){
+        discount(sum);
+        
+
+    })
+    
+    toShow(sum,particularUnitName);
+    toShowTotal(sum);
+    ShowFinalPrice(sum);
     
 }
-function toShow(sum)
+function toShow(sum,particularUnitName)
 {
-    const updateBoxName = document.getElementById("PriceInput")
-    const li = document.createElement("li");
-    sum = sum.toFixed(2);
-    
-    li.innerText = sum;
-    updateBoxName.innerHTML="Total price: "+sum;
-    // const li2 = document.createElement("li");
-    // DiscountPrice = DiscountPrice.toFixed(2);
-    // li2.innerText = DiscountPrice;
-    // updateBoxName.innerHTML = "Total: " + DiscountPrice;
-    console.log(sum)
-    
+    const updateBoxName = document.getElementById("PriceInput");
+    const updateBoxValueString = updateBoxName.innerText;
+    const updateBoxValue = parseFloat(updateBoxValueString);
+    // console.log(updateBoxValue)
+    sumNumber = parseFloat(sum);
+    sumNumber = sumNumber.toFixed(2);
+    updateBoxName.innerText = sumNumber+"TK";
+    const ItemName = document.getElementById("NameInput");
+    // console.log(particularUnitName)
+    const count = ItemName.childElementCount;
+    const p = document.createElement('p') ;
+    p.classList.add('my-4');
+    p.innerHTML = `${count + 1}.  ${particularUnitName} `;
+
+    ItemName.appendChild(p);
+    ItemName.style.cssText = `
+    font-size: 24px;
+    color: #111;
+    font-style: normal;
+    font-weight: 500;
+    padding-bottom: 24px;
+  `;
+
+
+}
+function toShowDiscount(discount)
+{
+    const updateBoxName2 = document.getElementById("DiscountPrice");
+    const updateBoxValue2 = updateBoxName2.innerText;
+    // console.log(updateBoxValue2)
+    discount = discount.toFixed(2);
+        updateBoxName2.innerText = discount+"TK";
 }
 
 function InputValue()
@@ -40,18 +72,41 @@ function InputValue()
         btnActive.disabled = true;
     }
 }
+function discount(value)
+{
+    if(value>200)
+    {
+        const discountPrice = (value/100)*20;
+        toShowDiscount(discountPrice);
+        const AfterDiscountPrice = value - discountPrice;
+        ShowFinalPrice(AfterDiscountPrice)
+    }
+    else
+    {
+        // toShowDiscount(value)
+        // ShowFinalPrice(value)
+    }
+}
 
-// document.getElementById("coupon-btn").addEventListener('click',function discount(value){
-//         if(value >0)
-//         {
-//             const discountValue = (value/100)*20;
-//             const total = value - discountValue;
-//             return total;
-//         }
+function toShowTotal(AfterDiscountPrice)
+{
+    const inputHTML = document.getElementById("PurchaseBtn");
     
-// })
-
-// function setToThePurchaseField(total)
-// {
-
-// }
+    // console.log(TotalPrice)
+    const btnActive = inputHTML.parentNode.childNodes[3];
+    // console.log(btnActive)
+    if(AfterDiscountPrice>0)
+    {
+        btnActive.removeAttribute('disabled');
+    }
+    else
+    {
+        btnActive.disabled = true;
+    }
+    }
+function ShowFinalPrice(OverallPrice)
+{
+    const TotalPriceString = document.getElementById("TotalPrice");
+    const TotalPrice = TotalPriceString.innerText;
+    TotalPriceString.innerText = OverallPrice + "TK";
+} 
